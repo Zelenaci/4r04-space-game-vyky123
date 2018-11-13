6103b3a81e9db8ffba32b2cbf6b03add0804aafe
Marrek Nožka <nozka@spseol.cz>
2018-11-13 11:03:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# PRG-SpaceGame
+
+Vesmírná střílečka
+
+[![Join the chat at gitter.im/spseol/Python](https://badges.gitter.im/spseol/PRG-No.svg)](https://gitter.im/spseol/Python?utm_source=share-link&utm_medium=link&utm_campaign=share-link)
+
+Vytvořte vesmírnou střílečku, dle své libosti a vkusu... :-)
+
+* https://naucse.python.cz/2018/installfest/
+* https://opengameart.org/content/space-shooter-redux
+* https://kenney.nl/
---
+++ game.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# Soubor:  kameny.py
+# Datum:   06.11.2018 10:01
+# Autor:   Marek Nožka, nozka <@t> spseol <d.t> cz
+# Licence: GNU/GPL
+############################################################################
+import pyglet
+import random
+from math import sin, cos, radians, pi
+
+window = pyglet.window.Window(1000, 800)
+batch = pyglet.graphics.Batch()   # pro optimalizované vyreslování objektů
+
+
+class SpaceObject(object):
+
+    def __init__(self, img_file,
+                 x=None, y=None,
+                 direction=None,
+                 speed=None, rspeed=None):
+
+        # nečtu obrázek
+        self.image = pyglet.image.load(img_file)
+        # střed otáčení dám na střed obrázku
+        self.image.anchor_x = self.image.width // 2
+        self.image.anchor_y = self.image.height // 2
+        # z obrázku vytvořím sprite
+        self.sprite = pyglet.sprite.Sprite(self.image, batch=batch)
+
+        # pokud není atribut zadán vytvořím ho náhodně
+        self._x = x if x is not None else random.randint(0, window.width)
+        self._y = y if y is not None else random.randint(0, window.height)
+        # musím správně nastavit polohu sprite
+        self.x = self._x
+        self.y = self._y
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, new):
+        self._x = self.sprite.x = new
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, new):
+        self._y = self.sprite.y = new
+
+    def tick(self, dt):
+        self.bounce()
+
+        # do promenne dt se uloží doba od posledního tiknutí
+        self.x += dt * self.speed * cos(pi / 2 - radians(self.direction))
+        self.sprite.x = self.x
+        self.y += dt * self.speed * sin(pi / 2 - radians(self.direction))
+        self.sprite.y = self.y
+        self.sprite.rotation += 0.01 * self.rspeed
+
+
+a = SpaceObject('SpaceShooterRedux/PNG/playerShip1_red.png')
+
+
+@window.event
+def on_draw():
+    window.clear()
+    batch.draw()
+
+
+pyglet.app.run()
